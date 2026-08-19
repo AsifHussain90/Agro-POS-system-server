5a852d056a2f3b4da529850f1f9fec30a2f1604f
AsifHussain90 <asifshiskin902@gmail.com>
2026-08-19 18:06:35 +0500

    fixed the user model bug

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -28,6 +28,7 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: [true, 'Password is required'],
+      select: false,
     },
     ChangePassword: {
       type: Boolean,

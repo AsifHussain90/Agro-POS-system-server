90f471ca4316be50e2cdaef703a87e810b175b53
AsifHussain90 <asifshiskin902@gmail.com>
2026-08-20 21:03:05 +0500

    updated the src

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import { farmerRequestRoutes } from './src/routes/farmerRequest.routes.js';
 import { productRoutes } from './src/routes/product.route.js';
 import { orderRoutes } from './src/routes/order.routes.js';
 import { adminRoutes } from './src/routes/admin.routes.js';
-import { superAdminRoutes } from './src/routes/superAdmin.routes.js';
+import { superAdminRoutes } from './src/routes/superAdmin.route.js';
 import { cartRoutes } from './src/routes/cart.routes.js';
 import { buyerProfileRoutes } from './src/routes/buyerProfile.routes.js';
 import farmerRoutes from './src/routes/farmer.route.js';
@@ -28,7 +28,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN?.split(',') || ['http://localhost:3000'],
+  origin: process.env.CORS_ORIGIN?.split(',') || ['http://localhost:5000'],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
